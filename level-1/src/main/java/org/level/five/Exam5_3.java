ac6b6656b357913671099f1a4fc22aed91b2ff12
Dang Van Thinh <dangvanthinh372004@gmail.com>
2024-11-01 08:37:12 +0700

    cap nhat bai tap java core 1

--- level-1/src/main/java/org/level/five/Exam5_3.java
+++ level-1/src/main/java/org/level/five/Exam5_3.java
@@ -8,7 +8,6 @@ public class Exam5_3 {
         int[] input = {1, 2, 3, 5,4,4,4,4,2, 4};
         System.out.println(outDuplicate(input));
     }
-
     static List<Integer> outDuplicate(int[] input){
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < input.length; i++) {
--- level-1/src/main/java/org/level/three/Exam3_5.java
+++ level-1/src/main/java/org/level/three/Exam3_5.java
@@ -4,10 +4,9 @@ import java.util.Arrays;
 
 public class Exam3_5 {
     public static void main(String[] args) {
-        int[] numbers = {1, 1, 1, 1};
+        int[] numbers = {1,5,4};
         System.out.println(smallest(numbers));
     }
-
     static int smallest(int[] input) {
         Arrays.sort(input);
         int smallNumber = 1;
